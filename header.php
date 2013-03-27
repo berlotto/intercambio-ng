3159fed0c657cb837e82d8e04c64b094b253ec31
Sérgio H. Berlotto Jr <sergio.berlotto@gmail.com>
2013-03-26 23:28:52 -0300

    Ajustes para tema do Intercambio

--- header.php
+++ header.php
@@ -41,17 +41,14 @@
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-
 			<?php $header_image = get_header_image();
 			if ( ! empty( $header_image ) ) : ?>
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<img src="<?php echo esc_url( $header_image ); ?>" 
-					width="960" height="156"
-					alt="" />
+					<img src="<?php echo esc_url( $header_image ); ?>" alt="" />
 				</a>
 			<?php endif; ?>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
